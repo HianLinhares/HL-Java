6c042cf004ef6ad507ffe5d592ac7b6a89a12dc9
Hian Linhares <hianfernando@hotmail.com>
2025-05-03 23:08:54 -0300

    reapredendo a usar o scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.print("Olá Hian Linhares");
+        System.out.println("Olá Hian Linhares");
+        Scanner hl = new Scanner(System.in);
+        System.out.println(somar(1,2));
+        System.out.print("digite um número:");
+        int numero = hl.nextInt();
+        System.out.println(numero);
     }
+
+
+
+
+
+    public static int somar (int numero1, int numero2){
+        int soma = numero1 + numero2;
+        return soma;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
