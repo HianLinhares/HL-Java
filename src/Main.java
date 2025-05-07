79cbe505a615e769af015b037f788467c82d6515
Hian Linhares <hianfernando@hotmail.com>
2025-05-06 23:23:43 -0300

    criando pasta de anotações do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-    Metodos sansungSmart = new Metodos(true,0,0);
-    sansungSmart.aumentarCanal();
-    sansungSmart.impressaoDeEstados();
+        Scanner hl = new Scanner(System.in);
+        Metodos sansungSmart = new Metodos(true,0,0);
+        sansungSmart.aumentarCanal();
+        sansungSmart.impressaoDeEstados();
 
 
     }
