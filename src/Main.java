eb5c721576d96aca9ea0338dd1eb09f8a297331d
Hian Linhares <hianfernando@hotmail.com>
2025-05-08 23:46:23 -0300

    condicional simples e condicional compoesta

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,24 @@ public class Main {
 
     public static void main(String[] args) {
        Scanner hl = new Scanner(System.in);
+
+       //exemplo de condicional simples
+       double saldo = 26.0;
+       double saldoSolicitado = 14.0;
+       if (saldoSolicitado<=saldo){
+           saldo = saldo - saldoSolicitado;
+           System.out.println("O saldo é: "+saldo);
+       }
+
+
+
+       //exemplo de condicional composta
+       int idadeHian = 9;
+       if (idadeHian==70){
+           System.out.println("HIAN TEM 30 ANOS");
+       }else{
+           System.out.println("HIAN NÂO TEM 30 ANOS");
+       }
     }
 
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -44,5 +44,4 @@ public int volume = 0;
 
 
 
-
 }
