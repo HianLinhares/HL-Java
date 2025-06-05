b60195ff76d528864b706c4ed2dae77bee80b250
Hian Linhares <hianfernando@hotmail.com>
2025-06-05 00:09:44 -0300

    Exercicio 06 - HL JAVA

---
+++ src/ExercicosJava/Ex06.java
@@ -0,0 +1,12 @@
+package ExercicosJava;
+
+import java.util.Scanner;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        Scanner hl = new Scanner(System.in);
+        System.out.print("INFORME UM NÚMERO:");
+        int numero = hl.nextInt();
+        System.out.println("SEU ANTECESSOR É "+(numero-1)+ " E SEU SUCESSOR É  "+(numero+1));
+    }
+}
