a7f727e4d63ea913d643e97d1c5ed4d58bb6718e
Hian Linhares <hianfernando@hotmail.com>
2025-06-24 22:43:51 -0300

    Aprendizados sobre Lista Encadeada - HL JAVA

---
+++ src/EstruturaDeDados/ListaEncadeada/ListaEncadeada.java
@@ -0,0 +1,33 @@
+package EstruturaDeDados.ListaEncadeada;
+
+public class ListaEncadeada <T> {
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada() {
+        this.referenciaEntrada = referenciaEntrada;
+    }
+
+    public boolean isEmpty(){
+        return referenciaEntrada == null ? true : false;
+    }
+
+    public int size(){
+        int tamanhoLista = 0;
+        No <T> referenciaAux = referenciaEntrada;
+        while(true){
+            if (referenciaAux!=null){
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo()!=null){
+                    referenciaAux = referenciaAux.getProximoNo();
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+
+        return tamanhoLista;
+    }
+
+}
---
+++ src/EstruturaDeDados/ListaEncadeada/No.java
@@ -0,0 +1,61 @@
+package EstruturaDeDados.ListaEncadeada;
+
+public class No<T> {
+    private T conteudo;
+    private No proximoNo;
+
+    public No(No proximoNo) {
+        this.proximoNo = null;
+    }
+
+    public No(T conteudo) {
+        this.conteudo = conteudo;
+        this.proximoNo = null;
+    }
+
+    public No(T conteudo, No proximoNo) {
+        this.conteudo = conteudo;
+        this.proximoNo = null;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo=" + conteudo +
+                ", proximoNo=" + proximoNo +
+                '}';
+    }
+
+    public String toStringEncadeado() {
+        String str = "No{" +
+                "conteudo=" + conteudo +
+                ", proximoNo=" + proximoNo +
+                '}';
+            if(proximoNo!=null){
+                str += "->" +proximoNo.toString();
+            }else{
+                str += "-> null";
+            }
+
+        return str;
+    }
+
+
+}
