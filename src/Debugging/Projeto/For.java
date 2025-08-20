a320130277b641469e3ac51bc5b9ecaf433fb0e4
Hian Linhares <hianfernando@hotmail.com>
2025-08-20 19:07:12 -0300

    Finalização do módulo de debugging

---
+++ src/Debugging/Projeto/For.java
@@ -0,0 +1,16 @@
+package Debugging.Projeto;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+
+            System.out.println("SOMA DA SOMA "+(i+i));
+        }
+
+        System.out.println("Hian Lindo lindo ");
+        System.out.println("Hian Lindo lindo ");
+
+
+    }
+}
